f7d92ee3ab823f974ba2cddf6c35617cd0701216
yvesdj <yves.dejonghe@student.ap.be>
2019-11-26 12:52:06 +0100

    Created empty TafelSelector activity

---
+++ app/src/main/java/com/example/appoftheyear/TafelSelector.java
@@ -0,0 +1,14 @@
+package com.example.appoftheyear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TafelSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tafel_selector);
+    }
+}
