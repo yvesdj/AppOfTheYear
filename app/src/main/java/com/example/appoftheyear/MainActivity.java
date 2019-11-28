c5a3ea1e5911c9029ae7c00f3f4b7230431350be
Philip De Rudder <philip_de_rudder@hotmail.com>
2019-11-28 08:59:18 +0100

    basis functionaliteit
    3 onclicks toegevoegs aan de main pagina

--- app/src/main/java/com/example/appoftheyear/MainActivity.java
+++ app/src/main/java/com/example/appoftheyear/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appoftheyear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void GaNaarBar(View view) {
+        Intent intent = new Intent(this, Bar.class);
+        startActivity(intent);
+
+
+
+    }
+
+    public void GaNaarKeuken(View view) {
+        Intent intent = new Intent(this, Keuken.class);
+        startActivity(intent);
+    }
+
+    public void GaNaarTafelSector(View view) {
+        Intent intent = new Intent(this, TafelSelector.class);
+        startActivity(intent);
     }
 }
