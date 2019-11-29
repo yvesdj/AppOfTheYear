378d553fb037a6b14dc1d721121c9fafa06a6a12
Yves De Jonghea <yves.dejonghe@student.ap.be>
2019-11-29 16:22:05 +0100

    Created Voorgerecht Class

---
+++ app/src/main/java/com/example/appoftheyear/Voorgerecht.java
@@ -0,0 +1,23 @@
+package com.example.appoftheyear;
+
+public class Voorgerecht {
+    private String _naam;
+    
+    public String Get_naam() {
+        return _naam;
+    }
+
+    public void Set_naam(String _naam) {
+        this._naam = _naam;
+    }
+
+    private int _prijs;
+
+    public int Get_prijs() {
+        return _prijs;
+    }
+
+    public void Set_prijs(int _prijs) {
+        this._prijs = _prijs;
+    }
+}
