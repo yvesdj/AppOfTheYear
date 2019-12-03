1679e3514037a0507c897d391e9618a1444f74a0
Yves De Jonghea <yves.dejonghe@student.ap.be>
2019-12-03 12:35:22 +0100

    Made fragments for Menu

---
+++ app/src/main/java/com/example/appoftheyear/MenuTabHoofdgerechten.java
@@ -0,0 +1,31 @@
+package com.example.appoftheyear;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MenuTabHoofdgerechten extends Fragment {
+
+
+    public MenuTabHoofdgerechten() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_tab_hoofdgerechten, container, false);
+    }
+
+}
