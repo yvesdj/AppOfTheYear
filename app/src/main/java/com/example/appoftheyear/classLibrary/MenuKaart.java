ecff574e522da205cdff16f10e31182c235ad97b
Yves De Jonghea <yves.dejonghe@student.ap.be>
2019-12-07 23:15:42 +0100

    Added full menu class

---
+++ app/src/main/java/com/example/appoftheyear/classLibrary/MenuKaart.java
@@ -0,0 +1,16 @@
+package com.example.appoftheyear.classLibrary;
+
+import java.util.LinkedList;
+
+public class MenuKaart {
+    private final LinkedList<MenuItem> MenuItems;
+
+    public MenuKaart(LinkedList<MenuItem> menuItems) {
+        MenuItems = menuItems;
+    }
+
+    public LinkedList<Voorgerecht> GetVoorgerechten(){
+        for
+    }
+
+}
